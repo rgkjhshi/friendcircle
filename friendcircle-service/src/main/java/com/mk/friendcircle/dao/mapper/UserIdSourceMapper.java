2db9da13d00c47addc3a324c946f925e08e8757f
shisong02 <shisong02@meituan.com>
2018-11-26 15:38:25 +0800

    删除多以方法

--- friendcircle-service/src/main/java/com/mk/friendcircle/dao/mapper/UserIdSourceMapper.java
+++ friendcircle-service/src/main/java/com/mk/friendcircle/dao/mapper/UserIdSourceMapper.java
@@ -13,15 +13,8 @@ public interface UserIdSourceMapper {
     /**
      * 添加记录
      *
+     * @param userIdSource 参数是为了回填id字段
      * @return 返回受影响的记录条数
      */
     int add(UserIdSource userIdSource);
-
-    /**
-     * 查询一条记录, 自行控制条件保证返回一条记录
-     *
-     * @param userIdSource 实体的非空属性会做为查询条件
-     * @return 查询到的结果, 无结果将返回null
-     */
-    UserIdSource queryOne(UserIdSource userIdSource);
 }
